9d2d4bf14c5ca9d38a2a955a88be82767e9e7af3
Gonza69420 <gonzalo.mato@ing.austral.edu.ar>
2023-11-02 17:35:44 -0300

    f

--- app/src/main/kotlin/electronicacli/api/ApiResponse.kt
+++ app/src/main/kotlin/electronicacli/api/ApiResponse.kt
@@ -1,4 +1,12 @@
 package electronicacli.api
+import electronicacli.utils.request
+
 
 class ApiResponse {
+    private val token : String = "";
+
+    fun login(username : String , password : String) {
+
+
+    }
 }
